c3249e009dd85761938e21d7e017f157bfc2f1f5
Franky Saxena <frankysaxena45@gmail.com>
2021-06-27 15:01:03 +0530

    first commit for todolist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// 🔴 Updated TODO LIST App with Add, Delete, Edit, LocalStorage using React and Hooks
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -15,3 +16,49 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//🔴 Build Awesome Todo App With REACT Using Hooks
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
